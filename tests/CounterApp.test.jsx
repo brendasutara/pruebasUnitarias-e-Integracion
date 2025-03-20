81e58d24d4704f0c43f991da4ce71797acfbb78d
Brenda Sutara <sutarabrendagmail.com>
2025-03-20 08:52:18 -0300

    CounterApp test

--- tests/CounterApp.test.jsx
+++ tests/CounterApp.test.jsx
@@ -11,13 +11,31 @@ describe('Pruebas en <CounterApp/>', () => {
      })
 
      test('debe de mostrar el valor inicial de 100', () => {
-        render( <CounterApp value={100} />)
+        render( <CounterApp value={100} />);
         expect( screen.getByText(100)).toBeTruthy();
      })
 
      test('debe de incrementar con el botón +1', () => {
-        render( <CounterApp value={initialValue} />)
-        fireEvent
+        render( <CounterApp value={initialValue} />);
+        fireEvent.click(screen.getByText('+1'));
+        expect(screen.getByText('11')).toBeTruthy();
      })  
+
+     test('debe de decrementar con el botón -1', () => {
+        render( <CounterApp value={initialValue} />);
+        fireEvent.click(screen.getByText('-1'));
+        expect(screen.getByText('9')).toBeTruthy();
+     })
+     
+     test('debe de funcionar el botón de Reset', () => {
+        render( <CounterApp value={200} />);
+        fireEvent.click(screen.getByText('-1'));
+        fireEvent.click(screen.getByText('-1'));
+        fireEvent.click(screen.getByText('-1'));
+
+        // fireEvent.click(screen.getByText('Reset'));
+        fireEvent.click(screen.getByRole('button', { name: 'btn-reset' }));
+        expect(screen.getByText(200)).toBeTruthy();
+     })
   
 })
